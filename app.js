0c8db0a2916546265d11c7425a4e2dcc383d1cf1
lucaspchartier <lucaspchartier@gmail.com>
2021-08-02 19:41:25 -0400

    fix(app.js): replace mongodb dependency with mongoose dependency in app.js
    also reformat pre-generated code from MongoDB
    Atlas

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient } = require('mongodb');
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-const client = new MongoClient(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-client.connect(err => {
-  client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
 });
 
 app.listen(4000, () => {
-    console.log("Now listening to requests on port 4000");
+  console.log("Now listening to requests on port 4000");
 });
