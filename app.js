919a6fd9218722817fecffcd7cd41275699f9ad0
lucaspchartier <lucaspchartier@gmail.com>
2021-08-02 18:57:13 -0400

    chore(app.js): require dotenv module for accessing API keys
    also use cors dependency to allow cross-origin
    requests

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.listen(4000, () => {
     console.log("Now listening to requests on port 4000");
