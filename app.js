3c786ebae5661d1de1232dca108bcb48c11f81cb
lucaspchartier <lucaspchartier@gmail.com>
2021-08-06 07:34:50 -0400

    chore(app.js): import graphqlHTTP function from express-graphql dependency

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema.js");
 
 const app = express();
