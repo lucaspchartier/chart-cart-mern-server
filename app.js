e3a5ef7d0559effe7ff9ec0c8f9d4e5758dd00df
lucaspchartier <lucaspchartier@gmail.com>
2021-08-06 07:33:13 -0400

    chore(app.js): require schema.js file into app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const schema = require("./schema/schema.js");
 
 const app = express();
 
